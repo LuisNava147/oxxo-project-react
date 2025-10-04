299119178c27af1179af46896234e858c950907b
LuisNava147 <luis.navas147@gmail.com>
2025-10-04 11:33:26 -0600

    Slidebar

---
+++ app/dasboard/_components/_sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+import { LuStore,LuUser,LuUsers,LuWheat,LuTruck } from "react-icons/lu"
+import NavItem from "./NavItem"
+
+export  default function Sidebar(){
+    return(
+        <nav className="w-[10vw] h-[90vh] bg-orange-200 flex flex-col items-center py-20" >
+            <NavItem icon={<LuStore className="text-4xl" />}path="/dashboard"/>
+            <LuTruck className="text-4xl"/>
+            <LuWheat className="text-4xl"/>
+            <LuUser className="text-4xl"/>
+            <LuUsers className="text-4xl"/>
+        </nav>
+    )
+}
---
+++ app/dasboard/layout.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Header from "./_components/Header";
+import Sidebar from "./_components/_sidebar/Sidebar";
+
+export default function layoutDashboard({children}: Readonly<{
+    children: React.ReactNode;
+   }>){
+return(
+    <div className="w-screen h-screen bg-orange-50">
+    <Header/>
+    <div className="flex flex-row items-center">
+    <Sidebar/>
+         {children}
+    </div>
+    </div>
+);
+}
