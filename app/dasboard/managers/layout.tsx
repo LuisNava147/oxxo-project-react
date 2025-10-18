de9da33a37119bb4bf1b6252b2111e39bd338e08
LuisNava147 <luis.navas147@gmail.com>
2025-10-18 14:36:15 -0600

    Feature: Stats Manager

--- app/dasboard/managers/layout.tsx
+++ app/dasboard/managers/layout.tsx
@@ -1,9 +1,10 @@
+
 import { ReactNode } from "react";
 import ManagerCards from "./_components/ManagerCards";
 
 
 
-export default function LayoutManager({children}:{children: ReactNode}){
+export default function LayoutManager({children, count}:{children: ReactNode, count: ReactNode}){
     return (
         <>
         <div className="w-4/12 max-h-[90vh] h-[90vh] overflow-hidden overflow-y-auto">
@@ -11,6 +12,7 @@ export default function LayoutManager({children}:{children: ReactNode}){
         </div>
         <div className="w-7/12">
             {children}
+            {count}
         </div>
         </>
     )
