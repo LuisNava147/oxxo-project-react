d21f43aa8323c6102b44fc9ae6c6e44e6b7e288a
LuisNava147 <luis.navas147@gmail.com>
2025-10-18 14:08:59 -0600

    Feature: Link a Location

---
+++ app/dasboard/managers/[id]/components/ManagerCard.tsx
@@ -0,0 +1,37 @@
+import { Manager } from "@/entities";
+import { CardBody, Card, CardHeader, Divider } from "@heroui/react";
+import Link from "next/link";
+
+export default function ManagerCard({manager}:{manager: Manager}){
+    return(
+        <>
+        <Card className="mx-20 py-2 text-center">
+        <CardHeader>
+        <p className="w-full"><b className="text-4xl">{manager.managerFullName}</b></p>
+        
+        </CardHeader>
+        <Divider/>
+        <CardBody className="flex flex-row flex-grow-0 items-center gap-10 justify-center">
+        <div className="flex flex-col text-lg">
+        <p className="w-full">Email: <b>{manager.managerEmail}</b></p>
+        <p className="w-full">Numero: <b>{manager.managerPhoneNumber}</b></p>
+        <p className={manager.location ? "" : "hidden"}> 
+        Tienda: {" "} <Link href={{pathname: `/dasboard`,
+                    query: {
+                        store: manager?.location?.locationId
+                    }
+                 }}>
+            <b className="underline">{manager?.location?.locationName}</b></Link></p>
+        </div>
+        {
+         manager.location ? ( 
+            <>
+               
+            </>
+            ) : <p className="w-full text-4xl">No tiene tienda</p>
+        }  
+        </CardBody>    
+        </Card>
+        </>
+    )
+}
--- app/dasboard/managers/[id]/page.tsx
+++ app/dasboard/managers/[id]/page.tsx
@@ -2,6 +2,7 @@ import { API_URL } from "@/constants"
 import { Manager } from "@/entities"
 import { authHeaders } from "@/helpers/authHeaders"
 import { Card,CardBody, Divider, CardHeader } from "@heroui/react"
+import ManagerCard from "./components/ManagerCard"
 
 export default async function ManagerPage({params}:{params :{id:string}}){
     const response = await fetch(`${API_URL}/managers/${params.id}`,{
@@ -14,24 +15,8 @@ export default async function ManagerPage({params}:{params :{id:string}}){
     })
     const data: Manager = await response.json()
 return (
-    <Card className="mx-20 py-2 bg-orange-50">
-            <CardHeader>
-            <p className="w-full">Nombre: <b>{data.managerFullName}</b></p>
-            
-            </CardHeader>
-            <Divider/>
-            <CardBody>
-            
-            <p className="w-full">Email: <b>{data.managerEmail}</b></p>
-            <p className="w-full">Numero: <b>{data.managerPhoneNumber}</b></p>
-            {
-             data.location ? ( 
-                <>
-                   <p> Tienda: <b>{data.location.locationName}</b></p>
-                </>
-                ) : <p>No tiene tienda</p>
-            }  
-            </CardBody>    
-    </Card>
+   <div>
+    <ManagerCard manager={data} />
+   </div>
 )
 }
