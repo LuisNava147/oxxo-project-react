59e9e7e575f0e72150fa2a262dbab4640e000afa
LuisNava147 <luis.navas147@gmail.com>
2025-10-19 22:43:26 -0600

    Feature: Providers Page

---
+++ app/dasboard/providers/[id]/page.tsx
@@ -0,0 +1,5 @@
+
+
+export default function ProviderPage(){
+    return "Show all providers"
+}
--- app/dasboard/providers/page.tsx
+++ app/dasboard/providers/page.tsx
@@ -1,7 +1,27 @@
+import { API_URL } from "@/constants";
+import { Provider } from "@/entities";
+import { authHeaders } from "@/helpers/authHeaders";
+import ProviderCard from "./_components/ProviderCard";
+import Link from "next/link";
 
 
-const ProviderPage = () =>{
-return null;
+const ProviderPage = async () =>{
+const response = await fetch(`${API_URL}/providers`, {
+    headers:{
+        ...authHeaders()
+    }
+}) 
+const providers:Provider[] = await response.json()
+return(
+    <div className="w-full flex-row flex-grow-0 flex-wrap px-10">
+        {providers.map((provider: Provider)=>(
+       <Link href={{pathname: `/dasboard/providers/${provider.providerId}`}}>   
+        <ProviderCard provider={provider} key={provider.providerId}/>
+        </Link>
+        ))}
+       
+    </div>
+)
 }
 
 export default ProviderPage;
