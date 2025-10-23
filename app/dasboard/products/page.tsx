f2790975ae5646701d85ab9174a3ad22f81e8093
LuisNava147 <luis.navas147@gmail.com>
2025-10-22 23:57:35 -0600

    Feature: Product Card en pestaña de product

--- app/dasboard/products/page.tsx
+++ app/dasboard/products/page.tsx
@@ -1,7 +1,27 @@
+import { API_URL } from "@/constants";
+import { Product } from "@/entities";
+import { authHeaders } from "@/helpers/authHeaders";
+import ProductCard from "./_components/ProductCard";
 
-
-const ProductsPage = ()=>{
-    return null;
+const ProductsPage = async ()=>{
+    const response = await fetch(`${API_URL}/products`, {
+        headers:{
+            ...authHeaders()
+        },
+        next: {
+            tags: ["dasboard:products"]
+        }
+    })
+    const products:Product[] = await response.json()
+    return (
+        <div>
+            {products.map((product) => {
+                return (
+                    <ProductCard product={product} />
+                )
+            })}
+        </div>
+    )
     }
     
     export default ProductsPage;
