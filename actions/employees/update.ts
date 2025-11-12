a4dc39318dd8505dc8d5a145fc62afce7fecf351
LuisNava147 <luis.navas147@gmail.com>
2025-11-11 21:05:28 -0600

    Fix: '$'ACTION para subir fotos y que no se borren

--- actions/employees/update.ts
+++ actions/employees/update.ts
@@ -5,18 +5,23 @@ import { revalidateTag } from "next/cache";
 
 
 export default async function updateEmployee(employeeId: string, formData: FormData){
-    formData.delete("$ACTION_REF_1")
-    formData.delete("$ACTION_1:1")
-    formData.delete("$ACTION_1:0")
+    const cleanData = new FormData()
+    for(const [key,value] of formData.entries()){
+        if(!key.startsWith("$")){
+            cleanData.append(key,value)
+        }
+    }
+    
 const response = await fetch(`${API_URL}/employees/${employeeId}`,{
     method: "PATCH",
     headers:{
         ...authHeaders()
     },
-    body: formData,
+    body: cleanData,
 })
-console.log(await response.json())
+//console.log(await response.json())
 if(response.status == 200) revalidateTag("dasboard:employees")
+if(response.status == 200) revalidateTag(`dasboard:employees:${employeeId}`)
 
     return;
 }
